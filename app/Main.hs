0b20e955d4e7f8dfd7596a3f714623a59a2dde88
kamoii <>
2019-06-28 02:54:49 +0900

    add-options Trying optparse-simple/applicative

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,53 @@ module Main where
 
 import Prelude()
 import Relude
+--
 import Lib
+--
+import Options.Applicative.Simple
 
+data PredefinedSelection
+  = PrimarySelection
+  | SecondarySelection
+  | ClipboardSelection
+  deriving (Eq, Show)
+
+data GlobalOpts = GlobalOpts
+  { targetSelections :: [PredefinedSelection]
+  } deriving Show
+
+data Command
+  = Echo
+  deriving Show
+
+{-
+標準 selection
+-}
 main :: IO ()
-main = absurd <$> copycat
+main = do
+  (opts, cmd) <-
+    simpleOptions "0.1.0"
+                  "X11 clipboard/selection utility."
+                  "desc"
+                  globalOptsParser $
+    do
+      addCommand
+        "echo"
+        "Echo the selection to stdout."
+        (const Echo)
+        (pure ())
+
+  print opts
+  print cmd
+  -- absurd <$> copycat
+
+globalOptsParser :: Parser GlobalOpts
+globalOptsParser =
+  let
+    targetSelections = fmap catMaybes $ sequenceA $ map optional
+      [ flag' PrimarySelection   (long "primary")
+      , flag' SecondarySelection (long "secondary")
+      , flag' ClipboardSelection (long "clipboard")
+      ]
+  in
+    GlobalOpts <$> targetSelections
